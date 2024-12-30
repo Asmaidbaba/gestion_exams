ac0f3671e6c48840f1e8c65dc15b49f62df59cf8
levi <fz.blz.2019@gmail.com>
2024-12-30 18:57:12 +0100

    modify the routes config

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,13 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'register::signup');
+
+$routes->get('register', 'register::index');
+$routes->post('register', 'register::store');
+
+$routes->get('login', 'Login::index');
+$routes->post('login', 'Login::auth');
+
+$routes->get('dashboard', 'Login::dashboard');
+$routes->get('logout', 'Login::logout');
+
